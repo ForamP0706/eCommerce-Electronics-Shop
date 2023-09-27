97f7a238e7a055921038457881481747f9d41883
ForamP0706 <patelforam530@gmail.com>
2023-09-27 16:23:58 -0400

    done

--- index.php
+++ index.php
@@ -46,14 +46,14 @@
 		                  <span class="fa fa-angle-right"></span>
 		                </a>
 		            </div>
-		            <h2>Monthly Top Sellers</h2>
+		            <h2>Some Featured Items</h2>
 		       		<?php
 		       			$month = date('m');
 		       			$conn = $pdo->open();
 
 		       			try{
 		       			 	$inc = 3;	
-						    $stmt = $conn->prepare("SELECT * from products");
+						    $stmt = $conn->prepare("SELECT * from products where category_id = 4");
 						    $stmt->execute();
 						    foreach ($stmt as $row) {
 						    	$image = (!empty($row['photo'])) ? 'images/'.$row['photo'] : 'images/noimage.jpg';
