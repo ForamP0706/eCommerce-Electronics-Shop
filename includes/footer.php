138f1ee077e61bf8af21e2c3683020783f3668a2
jaykp <jAY@github7>
2023-09-28 22:59:11 -0400

    Added ABout us, Updated Footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,15 +1,47 @@
 <footer class="main-footer">
     <div class="container">
-    <div class='box-body'>
-	    	<a class="btn btn-social-icon btn-facebook"><i class="fa fa-facebook"></i></a>
-	    	<a class="btn btn-social-icon btn-twitter"><i class="fa fa-twitter"></i></a>
-	    	<a class="btn btn-social-icon btn-instagram"><i class="fa fa-instagram"></i></a>
-	    	<a class="btn btn-social-icon btn-google"><i class="fa fa-google-plus"></i></a>
-	    	<a class="btn btn-social-icon btn-linkedin"><i class="fa fa-linkedin"></i></a>
-	  	</div>
-      <div class="pull-right hidden-xs">
-        <b>All rights reserved</b>
-      </div>
-      <strong>Copyright &copy; 2023 Brought to You By <a href="">Capstone</a></strong>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="social-icons">
+                    <a class="btn btn-social-icon btn-facebook"><i class="fa fa-facebook"></i></a>
+                    <a class="btn btn-social-icon btn-twitter"><i class="fa fa-twitter"></i></a>
+                    <a class="btn btn-social-icon btn-instagram"><i class="fa fa-instagram"></i></a>
+                    <a class="btn btn-social-icon btn-google"><i class="fa fa-google-plus"></i></a>
+                    <a class="btn btn-social-icon btn-linkedin"><i class="fa fa-linkedin"></i></a>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="copyright-text pull-right">
+                    <p>All rights reserved</p>
+                    <strong>&copy; 2023 Brought to You By <a href="#">Capstone</a></strong>
+                </div>
+            </div>
+        </div>
     </div>
 </footer>
+
+<style>
+    .main-footer {
+        background-color: #333; 
+        color: #fff; 
+        padding: 20px 0;
+    }
+
+    .social-icons a {
+        margin-right: 10px;
+        color: #fff; 
+    }
+
+    .copyright-text p {
+        margin-bottom: 5px;
+    }
+
+    .copyright-text a {
+        color: #fff; 
+        text-decoration: none;
+    }
+
+    .copyright-text a:hover {
+        text-decoration: underline; 
+    }
+</style>
