0e24c4c54001618e99aa52cedacaa85f51d4caed
jaykp <jAY@github7>
2023-10-04 16:01:03 -0400

    added components

---
+++ about_us.php
@@ -0,0 +1,58 @@
+<?php include 'includes/conn.php'; ?>
+<?php include 'includes/header.php'; ?>
+<body class="hold-transition skin-blue layout-top-nav">
+<div class="wrapper">
+
+    <?php include 'includes/navbar.php'; ?>
+
+    <div class="content-wrapper">
+        <div class="container">
+
+            <!-- Main content -->
+            <section class="content">
+                <div class="row">
+                    <div class="col-sm-9">
+                        
+                    <h1 style="text-align: center; font-weight: bold;">About Us</h1>
+
+<p>Welcome to  eCommerce Electronics, your trusted destination for all your technology needs. We pride ourselves on offering not only top-of-the-line products but also unmatched after-sales services. Our motto, "Customer is God, Be a Servant," embodies our commitment to serving you with the utmost dedication and care.</p>
+
+<h3>Our Commitment to After-Sales Service</h3>
+
+<p>What sets us apart is our unwavering dedication to providing exceptional after-sales services. We understand that your experience doesn't end with a purchase – it's just the beginning of a long-lasting relationship. Here's what you can expect from our outstanding after-sales support:</p>
+
+<em><b>-  Free Desktop PC Setup :</b></em>  <p>We go the extra mile to ensure your desktop PC is up and running smoothly. Our experts provide free setup services, so you can start using your new computer hassle-free.</p>
+
+<em><b>-  Convenient Laptop Repair :</b></em>  <p>Accidents happen, and we're here to help when they do. With our damaged laptop pickup and drop-off service, we make it easy for you to get your laptop repaired and back in your hands quickly.</p>
+
+<em><b>-  Round-the-Clock Support :</b></em>  <p>Our 24/7 customer support line ensures that help is just a phone call away. Whether you have a question, need technical assistance, or require urgent support, we're here for you day or night.</p>
+
+<h3>The Origin of Our Exceptional Service</h3> 
+
+<p>Our commitment to exceptional service was born from a personal experience that left a lasting impact. One of our team members faced a situation where their server was functioning perfectly, but hardware issues arose unexpectedly. Unfortunately, they couldn't find any service providers available at midnight to address the problem. This frustrating experience motivated us to create a service-oriented approach that would prevent others from enduring similar hardships.</p>
+
+ <h3>The Difference We Offer</h3> 
+
+<p>We understand that quality technology products can sometimes come with a hefty price tag. That's why we've worked tirelessly to bridge the gap between mainstream brands and budget-conscious consumers. Here's how we make a difference:</p>
+
+<em><b>-  Bulk Purchases :</b></em>  <p>We source products from reputable brands in bulk, allowing us to leverage economies of scale. This enables us to offer competitive prices and pass on substantial savings to you, our valued customer.</p>
+
+<em><b>-  Strategic Partnerships :</b></em>  <p>Through strategic partnerships with leading manufacturers and distributors, we have access to exclusive discounts and promotions. These partnerships further enhance our ability to provide you with the best deals on top-quality products.</p>
+
+<p>At  eCommerce Electronics, we're not just selling products; we're building relationships. We're dedicated to ensuring your technology needs are met with excellence and backed by unparalleled service. Thank you for choosing us as your trusted technology partner.</p>
+                    </div>
+                    <div class="col-sm-3">
+                        <?php include 'includes/sidebar.php'; ?>
+                    </div>
+                </div>
+            </section>
+
+        </div>
+    </div>
+
+    <?php include 'includes/footer.php'; ?>
+</div>
+
+<?php include 'includes/scripts.php'; ?>
+</body>
+</html>
