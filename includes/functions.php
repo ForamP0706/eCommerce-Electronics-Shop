6f1a8f6cfee5ecc2327a6ae8e4ccfee6e45ab595
ForamP0706 <patelforam530@gmail.com>
2023-10-09 15:45:20 -0400

    der

--- includes/functions.php
+++ includes/functions.php
@@ -3,50 +3,57 @@ function get_products($conn) {
     $sql = "SELECT products.*, categories.category_name AS CategoryName 
             FROM products 
             JOIN categories ON products.category_id = categories.id";
-    
-    $stmt = $conn->prepare($sql);
-    $stmt->execute();
-    
-    $result = $stmt->get_result();
+    $result = $conn->query($sql);
     $products = [];
 
-    while ($row = $result->fetch_assoc()) {
-        $products[] = $row;
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $products[] = $row;
+        }
     }
 
     return $products;
 }
 
-
 function get_product_by_id($conn, $id) {
-    $stmt =  $conn->prepare("SELECT products.*, categories.category_name AS CategoryName  
+    $sql = "SELECT products.*, categories.category_name AS CategoryName  
             FROM products 
             JOIN categories ON products.category_id = categories.id 
-            WHERE products.id = ?");
-  $stmt->bind_param("i", $id);
-  $stmt->execute();
-  $result = $stmt->get_result();
+            WHERE products.id = $id";
+    $result = $conn->query($sql);
 
-  if ($result->num_rows == 1) {
-      return $result->fetch_assoc();
-  } else {
-      return false;
-  }
+    if ($result->num_rows == 1) {
+        return $result->fetch_assoc();
+    } else {
+        return false;
+    }
 }
+
+function get_category_by_id($conn, $id) {
+    $sql = "SELECT * 
+            FROM categories             
+            WHERE categories.id = $id";
+            
+    $result = $conn->query($sql);
+
+    if ($result->num_rows == 1) {
+        return $result->fetch_assoc();
+    } else {
+        return false;
+    }
+}
+
 function get_categories($conn) {
     $sql = "SELECT * FROM categories";
-    
-    $stmt = $conn->prepare($sql);
-    $stmt->execute();
-    
-    $result = $stmt->get_result();
+    $result = $conn->query($sql);
     $categories = [];
 
-    while ($row = $result->fetch_assoc()) {
-        $categories[] = $row;
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $categories[] = $row;
+        }
     }
 
     return $categories;
 }
-
 ?>
