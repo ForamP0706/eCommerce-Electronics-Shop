1a49148769689f2df3a9d266e98c2856e7f34d38
ForamP0706 <patelforam530@gmail.com>
2023-10-13 22:20:49 -0400

    footer and index fixed

--- includes/footer.php
+++ includes/footer.php
@@ -1,27 +1,23 @@
-<footer class="footer bg-primary text-light py-3">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6">
-                <h5>Connect with Us</h5>
-                <div class="social-links mt-3">
-                    <a href="https://www.facebook.com/" class="text-light mx-2"><i class="fab fa-facebook-f"></i></a>
-                    <a href="https://twitter.com/" class="text-light mx-2"><i class="fab fa-twitter"></i></a>
-                    <a href="https://www.instagram.com/" class="text-light mx-2"><i class="fab fa-instagram"></i></a>
-                </div>
-            </div>
-            <div class="col-md-6">
-                <h5>Quick Links</h5>
-                <ul class="list-unstyled">
-                    <li><a href="index.php" class="text-light text-decoration-none">Home</a></li>
-                    <li><a href="#" class="text-light text-decoration-none">Products</a></li>
-                    <li><a href="#" class="text-light text-decoration-none">About Us</a></li>
-                    <li><a href="#" class="text-light text-decoration-none">Contact Us</a></li>
+
+        <footer class="footer bg-primary text-light py-3">
+            <div class="container mb-2"style="margin: auto; width:50%">
+                <ul class="list-unstyled text-center d-flex justify-content-between mt-3">
+                    <li style="margin: 0; padding: 0;"><a href="index.php" class="text-light text-decoration-none">Home</a></li>
+                    <li style="margin: 0; padding: 0;"><a href="#" class="text-light text-decoration-none">Products</a></li>
+                    <li style="margin: 0; padding: 0;"><a href="#" class="text-light text-decoration-none">About Us</a></li>
+                    <li style="margin: 0; padding: 0;"><a href="#" class="text-light text-decoration-none">Contact Us</a></li>
                 </ul>
             </div>
-        </div>
-        <hr class="mt-4">
-        <div class="text-center">
-            <p class="mb-0">E-store Electronics &copy; 2023</p>
-        </div>
-    </div>
-</footer>
+            <div class="social-links mt-3 text-center">
+                <a href="https://www.facebook.com/" class="text-light mx-2"><i class="fab fa-facebook-f"></i></a>
+                <a href="https://twitter.com/" class="text-light mx-2"><i class="fab fa-twitter"></i></a>
+                <a href="https://www.instagram.com/" class="text-light mx-2"><i class="fab fa-instagram"></i></a>
+            </div>
+            <div class="container">
+                <p class="m-0 text-center text-white">Copyright &copy; Your Website 2023</p>
+            </div>
+        </footer>
+       
+       </body>
+   </html>
+   
--- index.php
+++ index.php
@@ -35,7 +35,7 @@ include('includes/navbar.php');
             </a>
         </div>
         </section>
-        <section class="py-5">
+        <!-- <section class="py-5">
             <div class="container px-4 px-lg-5 mt-5">
                 <div class="row">
                     <div class="col-md-12">
@@ -57,7 +57,38 @@ include('includes/navbar.php');
                     </div>
                 </div>
             </div>
-        </section>
+        </section> -->
+        <section class="py-5">
+    <div class="container px-4 px-lg-5 mt-5">
+        <div class="row">
+            <div class="col-md-12">
+                <h3>All Products</h3>
+            </div>
+        </div>
+        <div class="row">
+            <?php
+            include('database/conn.php');
+            $sql = "SELECT * FROM products where category_id=2";
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
+                    echo '<div class="col-md-4">';
+                    echo '<div class="card">';
+                    echo '<img class="card-img-top" src="assets/images/products/' . $row['prod_img'] . '" alt="' . $row['prod_name'] . '">';
+                    echo '<div class="card-body">';
+                    echo '<h5 class="card-title">' . $row['prod_name'] . '</h5>';                 
+                    echo '<p class="card-text">Price: $' . $row['price'] . '</p>';
+                    echo '</div>';
+                    echo '</div>';
+                    echo '</div>';
+                }
+            }
+            ?>
+        </div>
+    </div>
+</section>
+    
     
         <?php include('includes/footer.php');
 
