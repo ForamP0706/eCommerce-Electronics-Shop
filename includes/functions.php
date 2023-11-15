5238766c1cf504c13ed79694b58158ddf19c472e
satin381 <satinderj6@gmail.com>
2023-11-15 00:14:33 -0500

    functions added
    new function added for new and current pages as required for retrieving and performing the proper action as per requirements!

--- includes/functions.php
+++ includes/functions.php
@@ -51,7 +51,7 @@ function update_customer($conn, $customer_id, $first_name, $last_name, $email, $
         die("Query failed: " . mysqli_error($conn));
     }
 
-    return true; // or you can customize the return value based on your needs
+    return true; 
 }
 function delete_customer($conn, $customer_id) {
     $query = "DELETE FROM customer WHERE id = $customer_id";
@@ -63,6 +63,74 @@ function delete_customer($conn, $customer_id) {
 
     return true;
 }
+function get_orders($conn) {
+    $sql = "SELECT * FROM order_table";
+    $result = mysqli_query($conn, $sql);
+
+    $orders = array();
+
+    if ($result) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            $orders[] = $row;
+        }
+    }
+
+    return $orders;
+}
+
+function get_order_details($conn, $orderId) {
+   
+    $orderId = mysqli_real_escape_string($conn, $orderId);
+
+    $query = "SELECT * FROM `order_table` WHERE ID = '$orderId'";
+    $result = mysqli_query($conn, $query);
+
+    if ($result && mysqli_num_rows($result) > 0) {
+       
+        $orderDetails = mysqli_fetch_assoc($result);
+        return $orderDetails;
+    } else {
+        return false; 
+    }
+}
+
+
+function update_order_status($conn, $orderId, $newStatus) {
+    // Prepare the SQL query
+    $sql = "UPDATE `order_table` SET `order_status` = ? WHERE `ID` = ?";
+
+    // Prepare the statement
+    $stmt = $conn->prepare($sql);
+
+    // Bind parameters
+    $stmt->bind_param("si", $newStatus, $orderId);
+
+    // Execute the statement
+    $result = $stmt->execute();
+
+    // Close the statement
+    $stmt->close();
+
+    return $result;
+}
+
+
+
+function delete_order($conn, $orderId) {
+    
+    $orderId = mysqli_real_escape_string($conn, $orderId);
+
+    $query = "DELETE FROM `order_table` WHERE ID = '$orderId'";
+    $result = mysqli_query($conn, $query);
+
+    // Check if the query was successful
+    if ($result) {
+        return true; // Order deleted successfully
+    } else {
+        return false; // Failed to delete order
+    }
+}
+
 function get_product_by_id($conn, $id) {
     $sql = "SELECT products.*, categories.category_name AS CategoryName  
             FROM products 
