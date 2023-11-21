170b2684eb7fba8c88c421d4f5e029b37fb17393
satin381 <satinderj6@gmail.com>
2023-11-20 22:25:04 -0500

    imprve

---
+++ about_us.php
@@ -0,0 +1,83 @@
+<?php include 'includes/header.php';
+ ?>
+<body class="hold-transition skin-blue layout-top-nav">
+<div class="wrapper">
+
+    <?php include 'includes/navbar.php'; ?>
+
+    <div class="content-wrapper">
+        <div class="container">
+
+            <section class="content">
+                <div class="row">
+                    <div class="col-sm-9">            
+                    <h1 style="text-align: center; font-weight: bold;">About Us</h1>
+                    <style>
+                        body {
+                            font-family: Arial, sans-serif;
+                            background-color: #f5f5f5;
+                        }
+
+                        .content-wrapper {
+                            background-color: #ffffff;
+                            padding: 20px;
+                            margin-top: 20px;
+                        }
+
+                        h1 {
+                            color: #333;
+                        }
+
+                        p {
+                            color: #666;
+                        }
+
+                        .banner {
+                            margin: 20px 0;
+                            background: url('https://picsum.photos/1200/200') center/cover no-repeat;
+                            height: 200px;
+                        }
+                    </style>
+
+                    <p>Welcome to  eCommerce Electronics, your trusted destination for all your technology needs. We pride ourselves on offering not only top-of-the-line products but also unmatched after-sales services. Our motto, "Customer is God, Be a Servant," embodies our commitment to serving you with the utmost dedication and care.</p>
+
+                    <h3>Our Commitment to After-Sales Service</h3>
+                    <div class="banner"></div>
+
+                    <p>What sets us apart is our unwavering dedication to providing exceptional after-sales services. We understand that your experience doesn't end with a purchase – it's just the beginning of a long-lasting relationship. Here's what you can expect from our outstanding after-sales support:</p>
+
+                    <em><b>-  Free Desktop PC Setup :</b></em>  <p>We go the extra mile to ensure your desktop PC is up and running smoothly. Our experts provide free setup services, so you can start using your new computer hassle-free.</p>
+                    <em><b>-  Online Cosultation :</b></em>  <p>Online step by step guidance </p>
+
+                    <em><b>-  Lifetime Technical Support :</b></em>  
+                    <p>Gain access to our expert technical support team throughout the lifespan of your product. Whether it's troubleshooting, advice, or technical guidance, we're here for you.</p>
+                    
+                    <em><b>- Personalized Consultation :</b></em>  
+                    <p>Book one-on-one consultations with our tech experts for personalized guidance and recommendations tailored to your needs and preferences.</p>
+                    
+                    <em><b>- Comprehensive Product Tutorials :</b></em>  
+                    <p>Access a library of in-depth tutorials and resources to help you maximize the potential of your purchase, ensuring you get the most out of your device.</p>
+                    
+                    <em><b>- Rapid Response Service :</b></em>  
+                    <p>Experience prompt and efficient service with our rapid response system, ensuring your concerns are addressed swiftly and effectively.</p>
+                    
+                    <em><b>- Exclusive Loyalty Benefits :</b></em> [Coming soon] 
+                    <p>Unlock special loyalty rewards, discounts, and exclusive offers as a token of our appreciation for choosing us as your tech partner.</p>
+
+                    <div class="banner"></div>
+
+
+                    </div>
+                    
+                </div>
+            </section>
+
+        </div>
+    </div>
+
+    <?php include 'includes/footer.php'; ?>
+</div>
+
+<?php include 'includes/scripts.php'; ?>
+</body>
+</html>
