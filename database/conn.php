7e5fedcf148ab0d8411c88e6fb4cf0b6d4e0e50b
satin381 <satinderj6@gmail.com>
2023-12-01 16:27:10 -0500

    these commented showing error
    thats why i commented them now it working fine , can you explain me why this isnt working if it is working in your ?

--- database/conn.php
+++ database/conn.php
@@ -1,9 +1,9 @@
 <?php
-//Stripe Credentials Configuration
-define("STRIPE_SECRET_API_KEY", "sk_test_51OHfmDIfuNxYRy4KbZyC5yckBSpGMkck89ltKULgfmcJTwtM1gtDSPEnwMTu3yqmUEHqmyP0zegFxPosVN8RIajP00MQOLhfaF");
-define("STRIPE_PUBLISHABLE_KEY", "pk_test_51OHfmDIfuNxYRy4Ki6laL1ARSyoSY2MUJnOUYDvTHQbNkdBcpo26inIWooN38JIc8VSgqNMooQEcs9zHRHcxLtOz00FKtRjWqz");
+// //Stripe Credentials Configuration
+// define("STRIPE_SECRET_API_KEY", "sk_test_51OHfmDIfuNxYRy4KbZyC5yckBSpGMkck89ltKULgfmcJTwtM1gtDSPEnwMTu3yqmUEHqmyP0zegFxPosVN8RIajP00MQOLhfaF");
+// define("STRIPE_PUBLISHABLE_KEY", "pk_test_51OHfmDIfuNxYRy4Ki6laL1ARSyoSY2MUJnOUYDvTHQbNkdBcpo26inIWooN38JIc8VSgqNMooQEcs9zHRHcxLtOz00FKtRjWqz");
 
-define('CURRENCY', 'CAD');
+// define('CURRENCY', 'CAD');
 
 
 $servername = "localhost";
