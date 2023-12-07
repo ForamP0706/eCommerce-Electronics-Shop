1c71515cd5ad658845d6327102134be3632eb06a
satin381 <satinderj6@gmail.com>
2023-12-07 12:11:01 -0500

    cleaned code

--- login.php
+++ login.php
@@ -1,4 +1,5 @@
-<?php include 'includes/header.php';
+<?php 
+include 'includes/header.php';
 include 'includes/navbar.php';
 ?>
 <?php
@@ -10,7 +11,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $role = $_POST['role'];
 
     if ($role === 'admin') {
-    
+
         $sql = "SELECT * FROM user WHERE UserName ='$username' AND Password = '$password'";
         $result = $conn->query($sql);
 
@@ -23,7 +24,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         }
     } elseif ($role === 'user') {
 
-    
+
         $sql = "SELECT * FROM customer WHERE email = ?";
         $stmt = $conn->prepare($sql);
         $stmt->bind_param('s', $username);
@@ -31,17 +32,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $result = $stmt->get_result();
         if ($result->num_rows == 1) {
             $row = $result->fetch_assoc();
-            
+
             // here we are Verifying the entered password against hashed password
             if (password_verify($password, $row['password'])) {
                 $_SESSION['customer_id'] = $row['id'];
                 $_SESSION['first_name'] = $row['first_name'];
                 $_SESSION['last_name'] = $row['last_name'];
                 $_SESSION['username'] = $username;
-               
-                
+
+
                 header('Location: index.php');
-           
+
                 exit;
             } else {
                 $login_error = "Invalid username or password.";
@@ -52,7 +53,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 }
 ?>
-
 <?php if (isset($login_error)) : ?>
     <p><?= $login_error; ?></p>
 <?php endif; ?>
@@ -84,22 +84,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         </div>
     </div>
 </div>
-
-
-
 <?php
 include 'includes/footer.php'; ?>
 <style>
-  
-
-        .footer {
-            background-color: #007BFF;
-            color: #fff;
-            padding: 20px 0;
-            position: absolute;
-            bottom: 0;
-            width: 100%;
-        }
-
-      
-    </style>
+    .footer {
+        background-color: #007BFF;
+        color: #fff;
+        padding: 20px 0;
+        position: absolute;
+        bottom: 0;
+        width: 100%;
+    }
+</style>
