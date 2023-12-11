599b7493a1a69f47a58b829f85d2cd3a322b876c
satin381 <satinderj6@gmail.com>
2023-12-10 19:26:02 -0500

    Product list UI improved
    proper aligning !

--- admin/product_list.php
+++ admin/product_list.php
@@ -18,21 +18,22 @@ $categories = get_categories($conn);
     <div class="container mt-5 mb-5 border rounded p-4 bg-white">
         <div class="table-responsive">
             <table class="table table-bordered table-hover">
-                <thead class="thead-dark">
+                <thead class="text-white bg-dark thead-dark">
                     <tr>
                         <th scope="col">Number</th>
                         <th scope="col">Name</th>
                         <th scope="col">Price</th>
                         <th scope="col">Category</th>
-                        <th scope="col">Quantity</th>
+                        <th scope="col" class="col-3">Quantity</th>
+                        <th scope="col">Action</th>
                     </tr>
                 </thead>
                 <tbody>
                     <?php foreach ($products as $product) : ?>
-                        <tr>
+                        <tr class="text-dark">
                             <td class="text-wrap"><?= $product['id']; ?></td>
                             <td class="text-wrap"><?= $product['prod_name']; ?></td>
-                            <td class="text-wrap"><?= $product['price']; ?></td>
+                            <td class="text-wrap">$<?= $product['price']; ?></td>
                             <td class="text-wrap">
                                 <?php
                                 // Fetch category name based on category_id
@@ -41,19 +42,29 @@ $categories = get_categories($conn);
                                 echo $category_name;
                                 ?>
                             </td>
-                            <td class="text-wrap"><?= $product['qty']; ?>
-                                <div>
-                                    <!-- form to update the quantity value  -->
-                                    <form method="post" action="update_quantity.php">
-                                        <input type="hidden" name="product_id" value="<?= $product['id']; ?>">
-                                        <input type="number" name="new_quantity" min="0" required placeholder="Enter new quantity">
-                                        <button type="submit" class="btn btn-primary btn-sm mt-3">Update</button>
+                            <td class="text-wrap align-middle">
+                                <div class="d-flex align-items-center">
+                                    <span class="mr-3 p-3"><?= $product['qty']; ?></span>
+                                    <form method="post" action="update_quantity.php" class="form-inline">
+                                        <div class="input-group">
+                                            <input type="hidden" name="product_id" value="<?= $product['id']; ?>">
+                                            <input type="number" name="new_quantity" min="0" required placeholder="New Quantity" class="form-control mr-2 p-3">
+                                            <div class="input-group-append d-flex align-items-center">
+                                                <button type="submit" class="btn btn-primary m-0">
+                                                    <i class="fas fa-sync-alt"></i> Update
+                                                </button>
+                                            </div>
+                                        </div>
                                     </form>
                                 </div>
                             </td>
-                            <td>
-                                <a href="product_edit.php?id=<?= $product['id']; ?>" class="btn btn-primary btn-sm">Edit</a>
-                                <a href="product_delete.php?id=<?= $product['id']; ?>" class="btn btn-danger btn-sm">Delete</a>
+                            <td class="align-middle">
+                                <a href="product_edit.php?id=<?= $product['id']; ?>" class="btn btn-warning btn-sm mb-0">
+                                    <i class="fas fa-edit"></i> Edit
+                                </a>
+                                <a href="product_delete.php?id=<?= $product['id']; ?>" class="btn btn-danger btn-sm mb-0" onclick="return confirm('Are you sure you want to delete this product?')">
+                                    <i class="fas fa-trash"></i> Delete
+                                </a>
                             </td>
                         </tr>
                     <?php endforeach; ?>
